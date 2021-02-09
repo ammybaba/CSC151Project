5ca5d445ce0ba62c7aa354db69f2b674d5bb8d77
Amelia Dominey <ameliabdominey@gmail.com>
2021-02-09 12:28:50 -0500

    Added new print line
    new line that prints "Progam modified, but able to revert back with GitHub Desktop :0"

--- Project_amelia_dominey.java
+++ Project_amelia_dominey.java
@@ -5,5 +5,6 @@ public class Project_amelia_dominey
    public static void main(String[] args)
    {
       System.out.println("Hello world :0");
+      System.out.print("Progam modified, but able to revert back with GitHub Desktop :0");
    }
 }
